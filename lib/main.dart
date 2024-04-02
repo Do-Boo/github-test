f44b3652df7ec12af36cefb6cab15be202a0ee8b
Do-Boo <ehdb135@gmail.com>
2024-04-02 20:09:32 +0900

    2024.4.2.(화) 21:00 Widget을 좌우로 배치

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Widget을 좌우로 배치하기"),
+          title: Text("Widget을 상하좌우로 배치하기"),
         ),
         body: Body(),
       ),
@@ -18,29 +18,41 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          Container(
-            width: 100,
-            height: 80,
-            color: Colors.red,
-            child: Text("Container 1"),
-          ),
-          Container(
-            width: 100,
-            height: 80,
-            color: Colors.green,
-            child: Text("Container 2"),
-          ),
-          Container(
-            width: 100,
-            height: 80,
-            color: Colors.blue,
-            child: Text("Container 3"),
-          ),
-        ],
-      )
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.red,
+              child: Text("Container 1"),
+            ),
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.green,
+              child: Text("Container 2"),
+            ),
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.blue,
+              child: Text("Container 3"),
+            ),
+          ],
+        ),
+        Container(
+          width: 300,
+          height: 120,
+          color: Colors.grey,
+          child: Text("Container 4"),
+        )
+      ]
     );
   }
 }
