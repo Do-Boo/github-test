bb2e4d73d70b857a62e9d27e2f49833155e523b8
Do-Boo <ehdb135@gmail.com>
2024-04-03 19:08:26 +0900

    2024.4.3.(수) 19:00 checkbox action

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,62 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: HomeWidget(),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(""),
+        ),
+        body: Body(),
+      ),
     ),
   );
 }
 
-class HomeWidget extends StatelessWidget {
-  const HomeWidget({super.key});
+class Body extends StatelessWidget {
+  const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Scaffold(
-        body: ContraintsWidget(),
-      ),
+    return Column(
+      children: [
+        TestCheckBox(),
+      ],
     );
   }
 }
 
-class ContraintsWidget extends StatelessWidget {
-  const ContraintsWidget({super.key});
+class TestCheckBox extends StatefulWidget {
+  const TestCheckBox({super.key});
+
+  @override
+  State<TestCheckBox> createState() => _TestCheckBoxState();
+}
+
+class _TestCheckBoxState extends State<TestCheckBox> {
+  late List<bool> values;
+
+  @override
+  void initState() {
+    super.initState();
+    values = [false, false, false];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Flexible(
-          child: Container(
-            color: Colors.blue,
-            child: Text(
-              "Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! ",
-              style: TextStyle(fontSize: 30),
-            ),
-          ),
-        ),
-        Flexible(
-          child: Container(
-            color: Colors.red,
-            child: Text(
-              "Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! ",
-              style: TextStyle(fontSize: 30),
-            ),
-          ),
-        ),
+        Checkbox(value: values[0], onChanged: ((value) => changeValue(0, value: value))),
+        Checkbox(value: values[1], onChanged: ((value) => changeValue(1, value: value))),
+        Checkbox(value: values[2], onChanged: ((value) => changeValue(2, value: value))),
       ],
     );
   }
+
+  void changeValue(int index, {bool? value = false}) {
+    setState(() {
+      values[index] = value!;
+    });
+  }
+
 }
