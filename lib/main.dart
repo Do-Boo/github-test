042cdabb712ce91091ce1d6fcb72d30501abcc4c
Do-Boo <ehdb135@gmail.com>
2024-04-03 13:10:37 +0900

    2024.4.3.(수) 13:00 스크롤

--- lib/main.dart
+++ lib/main.dart
@@ -18,41 +18,100 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        Row(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: 100,
-              height: 80,
-              color: Colors.red,
-              child: Text("Container 1"),
-            ),
-            Container(
-              width: 100,
-              height: 80,
-              color: Colors.green,
-              child: Text("Container 2"),
-            ),
-            Container(
-              width: 100,
-              height: 80,
-              color: Colors.blue,
-              child: Text("Container 3"),
-            ),
-          ],
-        ),
-        Container(
-          width: 300,
-          height: 120,
-          color: Colors.grey,
-          child: Text("Container 4"),
-        )
-      ]
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [ 
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 100,
+                height: 80,
+                color: Colors.red,
+                child: Text("Container 1"),
+              ),
+              Container(
+                width: 100,
+                height: 80,
+                color: Colors.green,
+                child: Text("Container 2"),
+              ),
+              Container(
+                width: 100,
+                height: 80,
+                color: Colors.blue,
+                child: Text("Container 3"),
+              ),
+            ],
+          ),
+          Container(
+            width: 300,
+            height: 120,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(vertical: 8),
+            child: Text("Container 4"),
+          ),
+          Container(
+            width: 300,
+            height: 120,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(vertical: 8),
+            child: Text("Container 4"),
+          ),
+          Container(
+            width: 300,
+            height: 120,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(vertical: 8),
+            child: Text("Container 4"),
+          ),
+          Container(
+            width: 300,
+            height: 120,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(vertical: 8),
+            child: Text("Container 4"),
+          ),
+          Container(
+            width: 300,
+            height: 120,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(vertical: 8),
+            child: Text("Container 4"),
+          ),
+          Container(
+            width: 300,
+            height: 120,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(vertical: 8),
+            child: Text("Container 4"),
+          ),
+          Container(
+            width: 300,
+            height: 120,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(vertical: 8),
+            child: Text("Container 4"),
+          ),
+          Container(
+            width: 300,
+            height: 120,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(vertical: 8),
+            child: Text("Container 4"),
+          ),
+          Container(
+            width: 300,
+            height: 120,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(vertical: 8),
+            child: Text("Container 4"),
+          ),
+        ]
+      ),
     );
   }
 }
