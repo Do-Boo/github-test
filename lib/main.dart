938c076905484827a3ae21f15a9e56bbb5c520f5
Do-Boo <ehdb135@gmail.com>
2024-04-04 14:48:38 +0900

    2024.4.4.(목) 15:00 BottonNavigetionBar test

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,57 @@
 import 'package:flutter/material.dart';
 
-const assetImagePath = 'assets/images';
-const bannerImage = '$assetImagePath/banner.png';
-
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(""),
-        ),
-        body: const Body(),
-      ),
+    const MaterialApp(
+      home: HomeWidget(),
     ),
   );
 }
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({super.key});
+
+  @override
+  State<HomeWidget> createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset(bannerImage),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(""),
+      ),
+      body: homeBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "search"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "person"),
+        ],
+        currentIndex: index,
+        onTap: (newIndex) => setState(() => index = newIndex),
+      ),
     );
+  }
+
+  Widget homeBody() {
+    switch (index) {
+      case 1:
+        return const Center(child: Icon(Icons.search, size: 100));
+      case 2:
+        return const Center(child: Icon(Icons.person, size: 100));
+      case 0:
+      default:
+        return const Center(child: Icon(Icons.home, size: 100));
+    }
   }
 }
