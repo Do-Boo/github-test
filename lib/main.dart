9ad2bba1e5f62dcbd58946a72c93d81d492927e9
Do-Boo <ehdb135@gmail.com>
2024-04-04 13:24:41 +0900

    2024.4.4.(목) 14:00 callback test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,185 +18,64 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        TestCheckBox(),
-        TestRadioButton(),
-        TestSlider(),
-        TestSwitch(),
-        TestPopupMenu(),
-      ],
-    );
+    return TestWidget();
   }
 }
 
-class TestCheckBox extends StatefulWidget {
-  const TestCheckBox({super.key});
+class TestWidget extends StatefulWidget {
+  const TestWidget({super.key});
 
   @override
-  State<TestCheckBox> createState() => _TestCheckBoxState();
+  State<TestWidget> createState() => _TestWidgetState();
 }
 
-class _TestCheckBoxState extends State<TestCheckBox> {
-  late List<bool> values;
-
-  @override
-  void initState() {
-    super.initState();
-    values = [false, false, false];
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Checkbox(
-            value: values[0],
-            onChanged: ((value) => changeValue(0, value: value))),
-        Checkbox(
-            value: values[1],
-            onChanged: ((value) => changeValue(1, value: value))),
-        Checkbox(
-            value: values[2],
-            onChanged: ((value) => changeValue(2, value: value))),
-      ],
-    );
-  }
-
-  void changeValue(int index, {bool? value = false}) {
-    setState(() {
-      values[index] = value!;
-    });
-  }
-}
-
-class TestRadioButton extends StatefulWidget {
-  const TestRadioButton({super.key});
-
-  @override
-  State<TestRadioButton> createState() => _TestRadioButtonState();
-}
-
-enum TestValue { test1, test2, test3 }
-
-class _TestRadioButtonState extends State<TestRadioButton> {
-  TestValue? selectValue;
+class _TestWidgetState extends State<TestWidget> {
+  int value = 0;
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        ListTile(
-          leading: Radio<TestValue>(
-            value: TestValue.test1,
-            groupValue: selectValue,
-            onChanged: (value) => setState(
-              () => selectValue = value!,
+        Text(
+          "Count: $value",
+          style: const TextStyle(fontSize: 30),
+        ),
+        TestButton(addCounter),
+      ],
+    );
+  }
+
+  void addCounter(int addValue) => setState(() => value = addValue + value);
+
+}
+
+class TestButton extends StatelessWidget {
+  const TestButton(this.callback, {super.key});
+
+  final Function(int) callback;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      width: double.infinity,
+      child: InkWell(
+        onTap: () => callback.call(1),
+        onDoubleTap: () => callback.call(5),
+        onLongPress: () => callback.call(10),
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+            decoration: BoxDecoration(border: Border.all()),
+            child: const Text(
+              "Up Counter",
+              style: TextStyle(fontSize: 24),
             ),
           ),
-          title: Text(TestValue.test1.name),
-          onTap: () => setState(() {
-            if (selectValue != TestValue.test1) {
-              selectValue = TestValue.test1;
-            }
-          }),
         ),
-        Radio<TestValue>(
-          value: TestValue.test2,
-          groupValue: selectValue,
-          onChanged: (value) => setState(
-            () => selectValue = value!,
-          ),
-        ),
-        Radio<TestValue>(
-          value: TestValue.test3,
-          groupValue: selectValue,
-          onChanged: (value) => setState(
-            () => selectValue = value!,
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class TestSlider extends StatefulWidget {
-  const TestSlider({super.key});
-
-  @override
-  State<TestSlider> createState() => _TestSliderState();
-}
-
-class _TestSliderState extends State<TestSlider> {
-  double value = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("${value.round()}"),
-        Slider(
-          value: value,
-          onChanged: (newValue) => setState(() => value = newValue),
-          divisions: 100,
-          max: 100,
-          min: 0,
-          label: value.round().toString(),
-          activeColor: Colors.red,
-        ),
-      ],
-    );
-  }
-}
-
-class TestSwitch extends StatefulWidget {
-  const TestSwitch({super.key});
-
-  @override
-  State<TestSwitch> createState() => _TestSwitchState();
-}
-
-class _TestSwitchState extends State<TestSwitch> {
-  bool value = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Switch(
-          value: value,
-          onChanged: (newValue) => setState(() => value = newValue,),
-        ),
-        CupertinoSwitch(
-          value: value,
-          onChanged: (newValue) => setState(() => value = newValue,),
-        ),
-      ],
-    );
-  }
-}
-
-class TestPopupMenu extends StatefulWidget {
-  const TestPopupMenu({super.key});
-
-  @override
-  State<TestPopupMenu> createState() => _TestPopupMenuState();
-}
-
-class _TestPopupMenuState extends State<TestPopupMenu> {
-  TestValue selectValue = TestValue.test1;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(selectValue.name),
-        PopupMenuButton(itemBuilder: (context) {
-          return TestValue.values.map((value) => PopupMenuItem(value: value, child: Text(value.name),),).toList();
-        },
-        onSelected: (newValue) => setState(() => selectValue = newValue),
-        ),
-      ],
+      ),
     );
   }
 }
