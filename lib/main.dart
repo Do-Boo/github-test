eccd681eade7f67c9d7c3ec764b486d3048cb090
Do-Boo <ehdb135@gmail.com>
2024-04-08 16:44:51 +0900

    2024.4.8.(월) 17:00 인스타그램 클론

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,90 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:vscode_flutter/game_body.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:vscode_flutter/body.dart';
 
-void main() {
-  runApp(const RSPApp());
-}
+void main() => runApp(const InstaCloneApp());
 
-class RSPApp extends StatelessWidget {
-  const RSPApp({super.key});
+class InstaCloneApp extends StatelessWidget {
+  const InstaCloneApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("가위 바위 보!"),
-        ),
-        body: const GameBody(),
-      ),
       theme: ThemeData(
-        useMaterial3: false,
+        colorScheme: const ColorScheme.light(
+          primary: Colors.white,
+          secondary: Colors.black,
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.grey,
+          type: BottomNavigationBarType.fixed,
+        ),
+        useMaterial3: true,
+      ),
+      home: const InstaCloneHome(),
+    );
+  }
+}
+
+class InstaCloneHome extends StatefulWidget {
+  const InstaCloneHome({super.key});
+
+  @override
+  State<InstaCloneHome> createState() => _InstaCloneHomeState();
+}
+
+class _InstaCloneHomeState extends State<InstaCloneHome> {
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Instagram",
+            style: GoogleFonts.lobsterTwo(color: Colors.black, fontSize: 32)),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: () {
+              print("Tab favorite pressed");
+            },
+            icon: const Icon(Icons.favorite_outline, size: 32),
+          ),
+          IconButton(
+            onPressed: () {
+              print("Tab paperplane pressed");
+            },
+            icon: const Icon(CupertinoIcons.paperplane, size: 32),
+          ),
+        ],
+      ),
+      body: InstaBody(index: index),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 28), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search, size: 28), label: "Search"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person, size: 28), label: "Profile"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications, size: 28),
+              label: "Notifications"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.more_vert, size: 28), label: "More"),
+        ],
+        currentIndex: index,
+        onTap: (newIndex) => setState(() => index = newIndex),
       ),
     );
   }
